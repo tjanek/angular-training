be8715bc06e5409ad493914930bd8ef1ac6a342c
Tomasz Janek <janek.tomasz@gmail.com>
2017-09-23 19:07:55 +0200

    example of property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit() {
   }
